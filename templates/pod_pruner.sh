fa387f9e15c098d6cf7b52b4fab777155441a50d
Ladislav Kolacek <lkolacek@redhat.com>
2021-11-01 13:50:10 +0100

    Fix pod-pruner cronjob and run it hourly
    Signed-off-by: Ladislav Kolacek <lkolacek@redhat.com>

--- templates/pod_pruner.sh
+++ templates/pod_pruner.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
 set -exuo pipefail
+delete_threshold=$(date --date="{{ osbs_pruner_pods_days_old }} days ago" --iso-8601=seconds --utc)
 OC='oc -n {{ osbs_ocp_namespace }}'
-
-delete_threshold=$(date --date="{{ osbs_pruner_pods_days_old }} days ago" --iso-8601=seconds
---utc)
 
 old_pods=$(
     $OC get pods -o json | \
@@ -15,11 +12,10 @@ if [ -z "$old_pods" ]
 then
     echo "Nothing to remove."
     exit 0
+else
+    echo -e "${old_pods}"
 fi
 
-echo -e "${old_pods}"
-echo
 count=$(wc -l <<<${old_pods})
-
 echo "SUMMARY: ${count} Pods will be removed"
 echo ${old_pods} | xargs -n 10 $OC delete pod
